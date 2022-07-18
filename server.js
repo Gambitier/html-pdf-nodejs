7b2d62cfa9e29ea4ba6201c7443c9c0b467722d9
gambitier <csegambitier@gmail.com>
2022-07-18 18:43:40 +0530

    open local html file

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const http = require('http');
 const puppeteer = require('puppeteer');
+const path = require('path');
+
+const absolutePath = path.resolve("./test.html");
+
+console.log(absolutePath);
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://news.ycombinator.com', {
+  await page.goto(absolutePath, {
     waitUntil: 'networkidle2',
   });
-  await page.pdf({path: 'hn.pdf', format: 'a4'});
+  await page.pdf({path: 'test.pdf', format: 'a4'});
 
   await browser.close();
 })();
